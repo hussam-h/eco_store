2f8340516094ec7691620e3f594da106f07db131
hussam-h <hussam1996r@gmail.com>
2024-05-12 20:23:25 +0300

    start app

---
+++ lib/eco_store.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class EcoStore extends StatelessWidget {
+  const EcoStore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Eco Store',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "Eco Store",
+            ),
+            centerTitle: true,
+          ),
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'eco_store.dart';
+
+void main() {
+  runApp(const EcoStore());
+}
+
+
